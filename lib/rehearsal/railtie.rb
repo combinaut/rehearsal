505b86a3a10966ec6edb562f80537894ad2d0f21
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2017-10-31 10:45:51 -0700

    Don’t inject middleware at a specific position
    It turns out `Rack::Lock` isn’t always present. Since we’re now saving a copy of the original request env before it hits Rails, we should get a fairly clean env hash for the second request. If necessary the host app can move the middleware to the desired location, but by default it will be appended.

--- lib/rehearsal/railtie.rb
+++ lib/rehearsal/railtie.rb
@@ -3,8 +3,7 @@ require 'rehearsal/middleware'
 module Rehearsal
   class Railtie < Rails::Railtie
     initializer "rehearsal.init" do |app|
-      # Insert as high up in the stack as we can so the env hash is as clean as possible for the preview request
-      app.config.middleware.insert_after('Rack::Lock', Rehearsal::Middleware)
+      app.config.middleware.use(Rehearsal::Middleware)
     end
   end
 end
